2dd7d22a775fe2712d65e0d206bdd1b102620c5c
sean <91675054+undont@users.noreply.github.com>
2026-07-04 11:57:30 +0100

    jiru: correct per-platform checksums

--- Formula/jiru.rb
+++ Formula/jiru.rb
@@ -10,17 +10,17 @@ class Jiru < Formula
       sha256 "dc2c1654bdcfe55bf999b5708bed4918285ceac54f63fc8daebc29a3a9238fe4"
     else
       url "https://github.com/undont/jiru/releases/download/v0.3.18/jiru-darwin-amd64"
-      sha256 "dc2c1654bdcfe55bf999b5708bed4918285ceac54f63fc8daebc29a3a9238fe4"
+      sha256 "01905ae810e26f2a6e2eb8a95c1c29c03d856d649c15f36052dbef1589f5db9a"
     end
   end
 
   on_linux do
     if Hardware::CPU.arm?
       url "https://github.com/undont/jiru/releases/download/v0.3.18/jiru-linux-arm64"
-      sha256 "dc2c1654bdcfe55bf999b5708bed4918285ceac54f63fc8daebc29a3a9238fe4"
+      sha256 "55aa637e23f236bbd571aaadc31d0b17429bcf47d870c9752efe111258b6bb3e"
     else
       url "https://github.com/undont/jiru/releases/download/v0.3.18/jiru-linux-amd64"
-      sha256 "dc2c1654bdcfe55bf999b5708bed4918285ceac54f63fc8daebc29a3a9238fe4"
+      sha256 "2a20d619cecd016eb9b1d5ed3c8114e32e4e2d7be8c50b4f43df701a1ce47d74"
     end
   end
 
